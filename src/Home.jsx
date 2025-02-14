a67d804d4bdb8d5f22eded1141346f5194f52dd9
Dhruv Malviya <dhruvnotfoundlol@gmail.com>
2025-02-14 11:58:39 +0530

    pompom

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Dhruv</h1>
+      <p>Contact: Dhruv@example.com</p>
+    </div>
+  );
+};
+
+export default Home;
